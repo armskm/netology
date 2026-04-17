567a4b001e078759ca14b2d97507a3577ff3f11e
armskm <avklementyev@yandex.ru>
2026-04-17 21:49:30 +0300

    add task 03-2-3

---
+++ ter-homework/03/src/disk_vm.tf
@@ -0,0 +1,47 @@
+# Создаём 3 одинаковых диска размером 1 ГБ
+resource "yandex_compute_disk" "storage_disk" {
+  count = 3
+
+  name     = "${var.storage_disk[0].name}-${count.index}"
+  type     = var.storage_disk[0].type
+  zone     = var.default_zone
+  size     = var.storage_disk[0].size
+}
+
+# Создаём одну ВМ storage
+resource "yandex_compute_instance" "storage" {
+  name        = var.vm_storage[0].name
+  hostname    = var.vm_storage[0].name
+  platform_id = var.vm_web_platform
+
+  resources {
+    cores         = var.vm_storage[0].cores
+    memory        = var.vm_storage[0].memory
+    core_fraction = var.vm_storage[0].core_fr
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = data.yandex_compute_image.ubuntu.image_id
+    }
+  }
+
+  # Динамический блок для подключения дисков
+  dynamic "secondary_disk" {
+    for_each = yandex_compute_disk.storage_disk[*].id
+    content {
+      disk_id = secondary_disk.value
+    }
+  }
+
+  scheduling_policy { preemptible = var.vm_preemptible }
+
+  network_interface {
+    subnet_id          = yandex_vpc_subnet.develop.id
+    nat                = var.vm_nat
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${local.public_key}"
+  }
+}
